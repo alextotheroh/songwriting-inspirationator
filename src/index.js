953688c3c317416fb7c635399aa5f37dbea1811d
Alex Totheroh <alextotheroh@gmail.com>
2019-04-05 23:51:40 -0700

    better organization

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import './styles/App.css';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
