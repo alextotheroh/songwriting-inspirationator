ac1d12981b58a29d5a530be4d0cf57e857e1fc95
Alex Totheroh <alextotheroh@gmail.com>
2019-04-07 00:42:43 -0700

    change favicon and tab title, use sass, define theme colors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
-import './styles/App.css';
+import './styles/index.scss';
+import './styles/App.scss';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
