790e8732b7139e278884dddd275bd18b11f76f51
Alex Totheroh <alextotheroh@gmail.com>
2019-04-13 12:01:19 -0700

    help button placed and styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 
 class Header extends Component {
   render() {
     return (
       <div>
-        <span className="Header-title">
-          Songwriting Inspirationator
-        </span>
+        <div className="Header-container">
+          <div className="Header-title">
+            Songwriting Inspirationator
+          </div>
+          <div className="Header-helpIconContainer">
+            <IconButton >
+              <HelpOutlineIcon color='primary'/>
+            </IconButton>
+          </div>
+        </div>
       </div>
     );
   }
