f9d75f64cdc68d7798c75740af16a4425c61fbcd
Alex Totheroh <alextotheroh@gmail.com>
2019-04-13 14:44:48 -0700

    can print template

--- src/components/GeneratedTemplate.js
+++ src/components/GeneratedTemplate.js
@@ -3,15 +3,23 @@ import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import PrintOutlinedIcon from '@material-ui/icons/PrintOutlined';
+import ReactToPrint from 'react-to-print';
 
 class GeneratedTemplate extends Component {
+
   render() {
     var t = this.props.template;
 
+    var printButton = 
+      <Button variant="contained" color="secondary" size="small" onClick={this.handlePrintClick} style={{marginLeft: '65%'}}>
+        <span className="Body-white">Print Template</span>&nbsp;&nbsp;
+        <PrintOutlinedIcon style={{color: "f0f0f0"}} />
+      </Button>; 
+
     return (
       <div>
-        <div className="GeneratedTemplate-container">
-          <Grid container spacing={2}>
+        <div className="GeneratedTemplate-container" ref={el => (this.componentRef = el)}>
+          <Grid container spacing={8}>
             <Grid item xs={6}>
               <div>
                 <div className="GeneratedTemplate-sectionTitle">Instruments</div>
@@ -31,10 +39,11 @@ class GeneratedTemplate extends Component {
             </Grid>
           </Grid>
         </div>
-        <Button variant="contained" color="secondary" size="small" onClick={this.handlePrintClick} style={{marginLeft: '65%'}}>
-          <span className="Body-white">Print Template</span>&nbsp;&nbsp;
-          <PrintOutlinedIcon style={{color: "f0f0f0"}} />
-        </Button>
+        
+        <ReactToPrint 
+          trigger={() => printButton}
+          content={() => this.componentRef}
+        />
       </div>
     );
   }
