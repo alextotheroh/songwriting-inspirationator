bc208efc4de2b3f613a56244abe2dd6da0de6388
Alex Totheroh <alextotheroh@gmail.com>
2020-02-23 15:41:43 -0800

    prettyfied scale and chord names

--- src/components/progressionator/ProgressionatorRoot.js
+++ src/components/progressionator/ProgressionatorRoot.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-import * as services from '../../backend/services/Services';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
-import InputLabel from '@material-ui/core/InputLabel';
-import TextField from '@material-ui/core/TextField';
-import Paper from '@material-ui/core/Paper';
 import WesternMusicScale from '../../backend/models/WesternMusicScale';
 import ProgressionatorService from '../../backend/services/ProgressionatorService';
 import { connect } from 'react-redux';
-import progressionatorReducer from '../../redux/reducers/progressionatorReducer';
 import { changeMode, changeRootNote } from '../../redux/actions/progressionatorActions';
 import PropTypes from 'prop-types';
 import Scale from '../../backend/models/Scale';
 import { Typography } from '@material-ui/core';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
 
 class ProgressionatorRoot extends Component {
 
@@ -28,47 +21,49 @@ class ProgressionatorRoot extends Component {
   render() {
 
     return (
-        <div className="theme-content-container">
-          <span>Root Note: &nbsp;&nbsp;&nbsp;</span>
-          <Select
-            className="ProgressionatorRoot-select theme-color-1"
-            value={this.props.rootNote}
-            onChange={this.handleRootNoteChange}
-            variant='outlined'
-          >
-            {this.westernMusicScale.getNotes().map((note) =>
-              <MenuItem value={note}>{note}</MenuItem>
-            )}
-          </Select>
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <span>Mode: &nbsp;&nbsp;&nbsp;</span>
+      <div className="theme-content-container">
+        <span className="theme-color-1 theme-font-2" style={{fontSize: "30px"}}>Root Note:</span>
+        <Select
+          className="ProgressionatorRoot-select"
+          value={this.props.rootNote}
+          onChange={this.handleRootNoteChange}
+          style={{marginLeft: '40px'}}
+        >
+          {this.westernMusicScale.getNotes().map((note) =>
+            <MenuItem value={note}>{note}</MenuItem>
+          )}
+        </Select><br/>
+
+        <div style={{marginTop: "10px"}}>  
+          <span className="theme-color-1 theme-font-2" style={{fontSize: "30px"}}>Mode:</span>
           <Select
             className="ProgressionatorRoot-select"
             value={this.props.modeName}
             onChange={this.handleModeChange}
-            input={
-              <OutlinedInput
-                name="mode"
-              />
-            }
+            style={{marginLeft: '92px'}}
           >
             {this.progressionatorService.getModeNames().map((modeName) =>
               <MenuItem value={modeName}>{modeName}</MenuItem>
             )}
           </Select>
-
-          <Typography>
+        </div>
+        
+        <div className="ProgressionatorRoot-scaleNotes">
+          <Typography component="span" className="theme-font-2">
             {this.getNotesForMode().map((note) => {
-            return <span>{note}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-            })}
-          </Typography>
-
-          <Typography>
-            {this.getDiatonicChordsForSelectedMode().map((chord) => {
-              return <span>{chord.getName()}&nbsp;&nbsp;&nbsp;</span>
+            return <span style={{marginRight: "50px"}}>{note}</span>
             })}
           </Typography>
         </div>
+
+        <div className="ProgressionatorRoot-chordNames">   
+          <Typography component="span" className="theme-font-2">
+            {this.getDiatonicChordsForSelectedMode().map((chord) => {
+              return <span style={{marginRight: "50px"}}>{chord.getName()}</span>
+            })}
+          </Typography>
+        </div> 
+      </div>
     );
   }
 
