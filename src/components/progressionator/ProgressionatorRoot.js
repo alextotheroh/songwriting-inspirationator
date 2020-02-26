b4439cdb26418ebb23376cb4db9501101992d8d8
Alex Totheroh <alextotheroh@gmail.com>
2020-02-26 02:06:55 -0800

    progression audoi loop working

--- src/components/progressionator/ProgressionatorRoot.js
+++ src/components/progressionator/ProgressionatorRoot.js
@@ -11,6 +11,7 @@ import PropTypes from 'prop-types';
 import Scale from '../../backend/models/Scale';
 import { Typography, Menu } from '@material-ui/core';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import StopIcon from '@material-ui/icons/Stop';
 import * as Tone from "tone";
 
 class ProgressionatorRoot extends Component {
@@ -46,28 +47,26 @@ class ProgressionatorRoot extends Component {
           className="ProgressionatorRoot-select"
           value={this.props.rootNote}
           onChange={this.handleRootNoteChange}
-          style={{marginLeft: '40px'}}
+          style={{marginLeft: '15px', marginRight: '60px'}}
         >
           {notesToChooseFrom.map((note) =>
             <MenuItem value={note.split(" ")[0]}>{note.split(" ")[0]}</MenuItem>
           )}
-        </Select><br/>
+        </Select>
 
-        <div style={{marginTop: "10px", marginBottom: "20px"}}>  
-          <span className="theme-color-1 theme-font-2" style={{fontSize: "30px"}}>Mode:</span>
-          <Select
-            className="ProgressionatorRoot-select"
-            value={this.props.modeName}
-            onChange={this.handleModeChange}
-            style={{marginLeft: '92px'}}
-          >
-            {this.progressionatorService.getModeNames().map((modeName) =>
-              <MenuItem value={modeName}>{modeName}</MenuItem>
-            )}
-          </Select>
-        </div>
+        <span className="theme-color-1 theme-font-2" style={{fontSize: "30px"}}>Mode:</span>
+        <Select
+          className="ProgressionatorRoot-select"
+          value={this.props.modeName}
+          onChange={this.handleModeChange}
+          style={{marginLeft: '15px'}}
+        >
+          {this.progressionatorService.getModeNames().map((modeName) =>
+            <MenuItem value={modeName}>{modeName}</MenuItem>
+          )}
+        </Select><br/><br/>
 
-        <Paper style={{padding: '40px 20px'}}>     
+        <Paper style={{padding: '12px 20px 10px 20px'}} elevation={8}>     
           <div className="ProgressionatorRoot-scaleNotes">
             <Typography component="span" className="theme-font-mono">
               {this.getNotesForMode().map((note) => {
@@ -78,7 +77,9 @@ class ProgressionatorRoot extends Component {
               </div>
             </Typography>
           </div>
+        </Paper>  
 
+        <Paper style={{padding: '40px 20px', marginTop: "20px"}} elevation={8}>    
           <Grid container spacing={1} className="ProgressionatorRoot-chordNames">
             {this.props.diatonicChordsForSelectedMode.map((chord) => {
               return (
@@ -93,49 +94,53 @@ class ProgressionatorRoot extends Component {
           </Grid> 
         </Paper>
 
-        <Paper style={{padding: "20px", marginTop: "20px"}}>
+        <Paper style={{padding: "20px", marginTop: "20px"}} elevation={8}>
           <Grid container>
             <Grid item xs>
-              <div className="ProgressionatorRoot-chordSlot" id="chord-slot-0" onClick={this.handleChordSlotClick}>
+              <div className="ProgressionatorRoot-chordSlot theme-font-mono" id="chord-slot-0" onClick={this.handleChordSlotClick}>
                 {this.state.progressionChords[0] ? this.state.progressionChords[0].getName() : null}
               </div>
             </Grid>
             <Grid item xs>
-              <div className="ProgressionatorRoot-chordSlot" id="chord-slot-1" onClick={this.handleChordSlotClick}>
+              <div className="ProgressionatorRoot-chordSlot theme-font-mono" id="chord-slot-1" onClick={this.handleChordSlotClick}>
                 {this.state.progressionChords[1] ? this.state.progressionChords[1].getName() : null}  
               </div>
             </Grid>
             <Grid item xs>
-              <div className="ProgressionatorRoot-chordSlot" id="chord-slot-2" onClick={this.handleChordSlotClick}>
-              {this.state.progressionChords[2] ? this.state.progressionChords[2].getName() : null}  
+              <div className="ProgressionatorRoot-chordSlot theme-font-mono" id="chord-slot-2" onClick={this.handleChordSlotClick}>
+                {this.state.progressionChords[2] ? this.state.progressionChords[2].getName() : null}  
               </div>
             </Grid>
             <Grid item xs>
-              <div className="ProgressionatorRoot-chordSlot" id="chord-slot-3" onClick={this.handleChordSlotClick}>
-              {this.state.progressionChords[3] ? this.state.progressionChords[3].getName() : null}  
+              <div className="ProgressionatorRoot-chordSlot theme-font-mono" id="chord-slot-3" onClick={this.handleChordSlotClick}>
+                {this.state.progressionChords[3] ? this.state.progressionChords[3].getName() : null}  
               </div>
             </Grid>
             <Grid item xs>
-              <div className="ProgressionatorRoot-chordSlot" id="chord-slot-4" onClick={this.handleChordSlotClick}>
-              {this.state.progressionChords[4] ? this.state.progressionChords[4].getName() : null}  
+              <div className="ProgressionatorRoot-chordSlot theme-font-mono" id="chord-slot-4" onClick={this.handleChordSlotClick}>
+                {this.state.progressionChords[4] ? this.state.progressionChords[4].getName() : null}  
               </div>
             </Grid>
             <Grid item xs>
-              <div className="ProgressionatorRoot-chordSlot" id="chord-slot-5" onClick={this.handleChordSlotClick}>
-              {this.state.progressionChords[5] ? this.state.progressionChords[5].getName() : null}  
+              <div className="ProgressionatorRoot-chordSlot theme-font-mono" id="chord-slot-5" onClick={this.handleChordSlotClick}>
+                {this.state.progressionChords[5] ? this.state.progressionChords[5].getName() : null}  
               </div>
             </Grid>
             <Grid item xs>
-              <div className="ProgressionatorRoot-chordSlot" id="chord-slot-6" onClick={this.handleChordSlotClick}>
-              {this.state.progressionChords[6] ? this.state.progressionChords[6].getName() : null}  
+              <div className="ProgressionatorRoot-chordSlot theme-font-mono" id="chord-slot-6" onClick={this.handleChordSlotClick}>
+                {this.state.progressionChords[6] ? this.state.progressionChords[6].getName() : null}  
               </div>
             </Grid>
             <Grid item xs>
-              <div className="ProgressionatorRoot-chordSlot" id="chord-slot-7" onClick={this.handleChordSlotClick}>
-              {this.state.progressionChords[7] ? this.state.progressionChords[7].getName() : null}  
+              <div className="ProgressionatorRoot-chordSlot theme-font-mono" id="chord-slot-7" onClick={this.handleChordSlotClick}>
+                {this.state.progressionChords[7] ? this.state.progressionChords[7].getName() : null}  
               </div>
             </Grid>
           </Grid>
+
+          <div className="ProgressionatorRoot-progressionPlayButton" id="tone-play-toggle-progression" onClick={this.handleProgressionPlayClick}>
+            {Tone.Transport.loop ? <StopIcon /> : <PlayArrowIcon />}
+          </div>
         </Paper>
 
         <Menu
@@ -171,7 +176,6 @@ class ProgressionatorRoot extends Component {
   }
 
   handlePlayScaleClick = () => {
-    Tone.Transport.stop();
     var scale = new Scale(this.props.rootNote, this.props.modeName)
     var volume = new Tone.Volume(-11).toMaster();
     var reverb = new Tone.JCReverb(0.4).connect(volume);
@@ -196,20 +200,12 @@ class ProgressionatorRoot extends Component {
     // add in upper octave
     notesToPlay.push(scale.getNotes()[0].split(" ")[0] + '5')
 
-    if (this.pattern) {
-      this.pattern.stop();
+    var time = Tone.Time('+0.001');
+
+    for (let note of notesToPlay) {
+      synthA.triggerAttackRelease(note, '8n', time);
+      time = time + Tone.Time('8n');
     }
-    this.pattern = new Tone.Pattern(function(time, note){
-      synthA.triggerAttackRelease(note, .25);
-    }, notesToPlay);
-    this.pattern.interval = "8n";
-    this.pattern.iterations = 8;
-
-    this.pattern.start(0);
-
-    Tone.Transport.loopEnd = '1m';
-    Tone.Transport.loop = false;
-    Tone.Transport.start();
   }
 
   handlePlayChordClick = (notes) => {
@@ -227,6 +223,50 @@ class ProgressionatorRoot extends Component {
     }).connect(volume);
 
     synth.triggerAttackRelease(notes.map(note => note.replace(/\s/g, '')), 1);
+  }
+
+  handleProgressionPlayClick = () => {
+    if (Tone.Transport.loop) {
+      Tone.Transport.toggle();
+      Tone.Transport.cancel();
+      Tone.Transport.loop = false;
+      this.forceUpdate();
+    } else {
+      var volume = new Tone.Volume(-8).toMaster();
+      var synth = new Tone.PolySynth(7, Tone.Synth, {
+        oscillator: {
+          type: 'triangle',
+        },
+        envelope: {
+          attack: 0.16,
+          decay: 2,
+          sustain: 0.9,
+          release: 1
+        }
+      }).connect(volume);
+
+      function triggerSynth(chord){
+        //the time is the sample-accurate time of the event
+        return (time) => synth.triggerAttackRelease(chord, 1, time)
+      }
+
+      var populatedChordSlotsCount = 0;
+      for (var i = 0; i < this.state.progressionChords.length; i++) {
+        if (this.state.progressionChords[i] != null) {
+          Tone.Transport.schedule(
+            triggerSynth(this.state.progressionChords[i].getNotes().map(note => note.replace(/\s/g, ''))), 
+            populatedChordSlotsCount
+          );
+          populatedChordSlotsCount++;
+        }
+        
+      }
+
+      Tone.Transport.loopEnd = populatedChordSlotsCount;
+      Tone.Transport.loop = true
+      Tone.Transport.toggle()
+      this.forceUpdate();
+    }
   }
 
   handleChordSlotClick = (e) => {
